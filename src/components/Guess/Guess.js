6547ad6d35b66a08478ca2e3a4ccc84f217b2bdb
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-01-19 22:13:51 +0700

    Add guess checking and status indication in Guess component
    This commit integrates the checking of guesses into the Guess component. It now uses the helper function 'checkGuess' to verify the user's guess against the answer. The results of this check are then used to dynamically generate the output, with each letter indicating its status (correct, misplaced, or incorrect).

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
+import { ANSWER } from "../../constants";
 
 function Guess({ guess }) {
-  const cells = range(5).map((i) => (
-    <span key={i} className="cell">
-      {guess ? guess.title[i] : ""}
-    </span>
-  ));
+  const isValidGuess = typeof guess !== "undefined";
+
+  let cells = range(5).map((i) => <span key={i} className="cell"></span>);
+
+  if (isValidGuess) {
+    const guessCheckResult = checkGuess(guess.title, ANSWER);
+
+    cells = guessCheckResult.map((letter, i) => (
+      <span className={`cell ${letter.status}`} key={i}>
+        {letter.letter}
+      </span>
+    ));
+  }
 
   return <p className="guess">{cells}</p>;
 }
