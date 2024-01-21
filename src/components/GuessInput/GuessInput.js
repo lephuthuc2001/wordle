1f9a7da1fad457b5d3c21859ac7587718f18b05d
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-01-21 12:38:34 +0700

    Add invalid input handling for GuessInput component
    A new function, handleInvalidGuess, has been added to the GuessInput component. This function is triggered onInvalid and shows a custom message prompting users to enter a 5 letter word. This approach provides a more user-friendly experience by giving users direct feedback on their input.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -11,12 +11,18 @@ function GuessInput({ addGuess, disabled }) {
   };
 
   const handleGuessInputChange = (e) => setGuess(e.target.value.toUpperCase());
+
+  const handleInvalidGuess = (e) => {
+    e.target.setCustomValidity("Please enter a 5 letter word");
+  };
+
   return (
     <form onSubmit={handleSubmit} className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         value={guess}
         onChange={handleGuessInputChange}
+        onInvalid={handleInvalidGuess}
         id="guess-input"
         type="text"
         disabled={disabled}
